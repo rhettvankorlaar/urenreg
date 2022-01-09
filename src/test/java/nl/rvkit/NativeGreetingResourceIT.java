2fe4d3577221a0cc5300f1427b9de0704414df98
quarkusio <no-reply@quarkus.io>
2022-01-09 00:59:01 +0000

    Initial commit

---
+++ src/test/java/nl/rvkit/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package nl.rvkit;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
